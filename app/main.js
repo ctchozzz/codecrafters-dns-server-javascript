57528e28f941d03f76e2847d5ea02020ae58808d
ctchozzz <ctchozzz@ctchozzz-ubuntu24.04>
2026-01-16 22:44:50 +0800

    test

--- app/main.js
+++ app/main.js
@@ -76,39 +76,41 @@ function extractDomainName(buf) {
   let res = [];
   let pos = 0; // position of the "length of label"
   while (true) {
-    const len = buf.readUInt8(pos);
-    if (len === 0) {
-      res.push(0x00);
-      break;
+    //   const len = buf.readUInt8(pos);
+    //   if (len === 0) {
+    //     res.push(0x00);
+    //     break;
+    //   }
+    //   const sl = buf.slice(pos, pos + len + 1);
+    //   res.push(...sl);
+    //   pos += len + 1; // move to next label
+    // }
+    // return res;
+
+    if (isCompressed(buf.subarray(pos))) {
+      // starts with 11, label is compressed
+      const pointer = buf.readUInt16BE(pos) & 0x3fff; // AND with 0011 1111 1111 1111 to get the pointer
+      console.log("pointer:", pointer);
+      const pointedName = extractDomainName(buf.subarray(pointer));
+      console.log("pointedName:", pointedName);
+      res.concat(Buffer.concat([buf.slice(0, pos), pointedName]));
+
+      // for compressed message, label takes up 2 bytes (11 + 14 bit pointer)
+      pos += 2;
+    } else {
+      const len = buf.readUInt8(pos);
+      if (len === 0) {
+        res.push(0x00);
+        break;
+      }
+      const sl = buf.slice(pos, pos + len + 1);
+      res.push(...sl);
+      pos += len + 1; // move to next label
     }
-    const sl = buf.slice(pos, pos + len + 1);
-    res.push(...sl);
-    pos += len + 1; // move to next label
   }
+
   return res;
-
-  // if (isCompressed(buf.subarray(pos))) {
-  //   // starts with 11, label is compressed
-  //   const pointer = buf.readUInt16BE(pos) & 0x3fff; // AND with 0011 1111 1111 1111 to get the pointer
-  //   console.log("pointer:", pointer);
-  //   const pointedName = extractDomainName(buf.subarray(pointer));
-  //   console.log("pointedName:", pointedName);
-  //   res.concat(Buffer.concat([buf.slice(0, pos), pointedName]));
-
-  //   // for compressed message, label takes up 2 bytes (11 + 14 bit pointer)
-  //   pos += 2;
-  // } else {
-  //   const len = buf.readUInt8(pos);
-  //   if (len === 0) {
-  //     break;
-  //   }
-  //   res = res.concat(buf.slice(pos, pos + len + 1));
-  //   pos += len + 1;
-  // }
 }
-
-//   return res;
-// }
 
 function uint8ToBinaryString(byte) {
   return byte.toString(2).padStart(8, "0");
